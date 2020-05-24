ebadb2ecba33efe488fb910110a020bb06e45f64
alihanakcm <alihanakcm@gmail.com>
2020-05-24 18:57:06 +0300

    feat: added routing

---
+++ src/routes/Routes.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {Home, Login, Register, Entry} from '../components';
+const Stack = createStackNavigator();
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Entry"
+          component={Entry}
+          options={{
+            title: 'Ailem Güvende',
+            headerTintColor: 'white',
+            headerStyle: {backgroundColor: 'black'},
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: 'Ailem Güvende',
+            headerTintColor: 'white',
+            headerStyle: {backgroundColor: 'black'},
+          }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{
+            title: 'Ailem Güvende',
+            headerTintColor: 'white',
+            headerStyle: {backgroundColor: 'black'},
+          }}
+        />
+        <Stack.Screen
+          name="Main"
+          component={Home}
+          options={{
+            title: 'Ailem Güvende',
+            headerTintColor: 'white',
+            headerStyle: {backgroundColor: 'black'},
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
