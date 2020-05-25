18bb9ad4e28b1448c0cd624436abf7e6228c7441
ismet kizgin <info@ismetkizgin.com>
2020-05-26 00:59:03 +0300

    fix: fix for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
-import {StackActions} from '@react-navigation/native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { commonStyle, colorStyle } from '../util'
+import { StackActions } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import {commonStyle, colorStyle} from '../util';
-
-export const Login = ({navigation}) => {
+const storeData = async (value) => {
+  try {
+    await AsyncStorage.setItem('@token', value)
+    console.log('token', await AsyncStorage.getItem('@token'));
+  } catch (e) {
+  }
+}
+export const Login = ({ navigation }) => {
   return (
     <View style={commonStyle.centerMain}>
-      <View
-        style={[commonStyle.centerContainer, {flex: 1, position: 'absolute'}]}>
+      <View style={[commonStyle.centerContainer, { flex: 1, position: "absolute" }]}>
         <Text style={commonStyle.h1}>Giriş Yap</Text>
-        <TextInput
-          style={commonStyle.textInput}
+        <TextInput style={commonStyle.textInput}
           placeholderTextColor={colorStyle.textColor}
-          placeholder="T.C. No"
-        />
-        <TextInput
-          style={commonStyle.textInput}
+          placeholder="T.C. No" />
+        <TextInput style={commonStyle.textInput}
           placeholderTextColor={colorStyle.textColor}
-          placeholder="Şifre"
-        />
+          placeholder="Şifre" />
         <TouchableOpacity style={commonStyle.button}>
           <Text
-            onPress={() => navigation.dispatch(StackActions.replace('drawer'))}
+            onPress={() => {
+              storeData("ismet")
+              navigation.dispatch(StackActions.replace('drawer'))
+            }}
             style={commonStyle.buttonText}>
             GİRİŞ YAP
-          </Text>
+        </Text>
         </TouchableOpacity>
         <TouchableOpacity>
           <Text style={commonStyle.textLink}>Şifremi Unuttum</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-          <Text
-            onPress={() => {
-              navigation.dispatch(StackActions.replace('drawer'));
-            }}
-            style={commonStyle.textLink}>
-            Kayıt Ol
-          </Text>
+          <Text onPress={() => { Actions.Register }} style={commonStyle.textLink}>Kayıt Ol</Text>
         </TouchableOpacity>
       </View>
     </View>
