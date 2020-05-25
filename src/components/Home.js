06e67b48aa8ee80db1f7d96cf55e794bf53eb9da
alihanakcm <alihanakcm@gmail.com>
2020-05-26 02:47:39 +0300

    feat: added icon package

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import {Button, View, Text} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-
+import {Icon} from '../components';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 export const Home = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <FontAwesome5 name={'comments'} />
+
       <Text>Home Screen</Text>
     </View>
   );
