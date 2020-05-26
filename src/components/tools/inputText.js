70eb7220d3aedaffb78a735ccbebbcdf182ec4d2
ismet kizgin <info@ismetkizgin.com>
2020-05-26 04:04:15 +0300

    feat: add input component

---
+++ src/components/tools/inputText.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TextInput, View } from "react-native";
+import { commonStyle } from '../../util'
+
+const propTypes = {
+    mapElement: PropTypes.func,
+    onSubmitEditing: PropTypes.func,
+    onChangeText: PropTypes.func,
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    maxLength: PropTypes.number,
+    keyboardType: PropTypes.string,
+    secureTextEntry: PropTypes.bool,
+    label: PropTypes.string
+};
+
+const defaultProps = {
+    mapElement: (n) => { },
+    onSubmitEditing: () => { },
+    onChangeText: () => { },
+    value: "",
+    placeholder: "",
+    maxLength: 200,
+    keyboardType: "default",
+    secureTextEntry: false,
+    label: ""
+};
+
+export class InputText extends Component {
+
+    state = {
+        value: ""
+    }
+
+    componentDidMount() {
+        this.setState({
+            value: this.props.value
+        });
+    }
+
+    onChangeText = (value) => {
+        this.setState({
+            value
+        }, () => {
+            this.props.onChangeText(value);
+        })
+    }
+
+    render() {
+        const { placeholder, secureTextEntry, keyboardType, maxLength, value, onChangeText, onSubmitEditing } = this.props;
+        return (
+            <View>
+                <TextInput
+                    style={commonStyle.inputBox}
+                    underlineColorAndroid="rgba(0,0,0,0)"
+                    placeholder={placeholder}
+                    placeholderTextColor="rgba(255,255,255,0.8)"
+                    selectionColor="#999999"
+                    secureTextEntry={secureTextEntry}
+                    keyboardType={keyboardType}
+                    maxLength={maxLength}
+                    returnKeyType="next"
+                    value={this.state.value}
+                    onSubmitEditing={onSubmitEditing}
+                    onChangeText={this.onChangeText} />
+            </View>
+        );
+    }
+}
+
+InputText.defaultProps = defaultProps;
+
+InputText.propTypes = propTypes;
