7e8da4aaa777ab50d20fd29bf586e57616131213
alihanakcm <alihanakcm@gmail.com>
2020-05-27 00:11:55 +0300

    fix: instant eathquakes page fixed

--- src/pages/InstantEarthquakes.js
+++ src/pages/InstantEarthquakes.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import {SafeAreaView, FlatList, Text, View, StyleSheet} from 'react-native';
-import {commonStyle} from '../util';
+import {commonStyle, instantEarthquakes} from '../util';
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    id: '1',
     location: 'Ankara',
     date: '12.12.2012',
-    time: '15:18:12:00',
+    time: '15:18:12',
     depth: '12',
     size: '10',
   },
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    id: '2',
     location: 'Ankara',
     date: '12.12.2012',
-    time: '15:18:12:00',
+    time: '15:18:12',
     depth: '12',
     size: '10',
   },
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    id: '3',
     location: 'Ankara',
     date: '12.12.2012',
-    time: '15:18:12:00',
+    time: '15:18:12',
+    depth: '12',
+    size: '10',
+  },
+  {
+    id: '4',
+    location: 'Ankara',
+    date: '12.12.2012',
+    time: '15:18:12',
+    depth: '12',
+    size: '10',
+  },
+  {
+    id: '5',
+    location: 'Ankara',
+    date: '12.12.2012',
+    time: '15:18:12',
+    depth: '12',
+    size: '10',
+  },
+  {
+    id: '6',
+    location: 'Ankara',
+    date: '12.12.2012',
+    time: '15:18:12',
+    depth: '12',
+    size: '10',
+  },
+  {
+    id: '7',
+    location: 'Ankara',
+    date: '12.12.2012',
+    time: '15:18:12',
     depth: '12',
     size: '10',
   },
@@ -31,21 +63,24 @@ const DATA = [
 export const InstantEarthquakes = () => {
   return (
     <SafeAreaView style={commonStyle.container}>
-      <View style={styles.headers}>
-        <Text style={styles.headerText}>Yer</Text>
-        <Text style={styles.headerText}>Tarih</Text>
-        <Text style={styles.headerText}>Saat</Text>
-        <Text style={styles.headerText}>Derinlik</Text>
-        <Text style={styles.headerText}>Büyüklük</Text>
+      <View style={instantEarthquakes.mainHeader}>
+        <Text style={instantEarthquakes.headerText}>SON DEPREMLER</Text>
+      </View>
+      <View style={instantEarthquakes.headers}>
+        <Text style={instantEarthquakes.headersText}>Yer</Text>
+        <Text style={instantEarthquakes.headersText}>Tarih</Text>
+        <Text style={instantEarthquakes.headersText}>Saat</Text>
+        <Text style={instantEarthquakes.headersText}>Derinlik</Text>
+        <Text style={instantEarthquakes.headersText}>Büyüklük</Text>
       </View>
       <FlatList
         data={DATA}
         renderItem={({item}) => (
-          <View style={styles.itemText}>
+          <View style={instantEarthquakes.item}>
             <Text>{item.location}</Text>
             <Text>{item.date}</Text>
-            <Text>{item.time}</Text>
-            <Text>{item.depth}</Text>
+            <Text style={{paddingRight: 30}}>{item.time}</Text>
+            <Text style={{paddingRight: 50}}>{item.depth}</Text>
             <Text>{item.size}</Text>
           </View>
         )}
@@ -54,17 +89,3 @@ export const InstantEarthquakes = () => {
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  headers: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-  },
-  headerText: {fontSize: 20, color: 'gray'},
-  itemText: {
-    paddingRight: 30,
-    paddingLeft: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
