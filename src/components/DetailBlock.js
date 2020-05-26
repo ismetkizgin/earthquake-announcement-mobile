65ebca43e99737b7f27645553e552bbb1dc0793b
alihanakcm <alihanakcm@gmail.com>
2020-05-27 01:30:13 +0300

    fix: detailBlock component fixed

--- src/components/DetailBlock.js
+++ src/components/DetailBlock.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { itemStyle } from '../util';
+import React, {Component} from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {itemStyle} from '../util';
 import PropTypes from 'prop-types';
 
 const propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
   source: PropTypes.string,
+  navigation: PropTypes.navigate,
 };
 
 const defaultProps = {
@@ -17,15 +18,16 @@ const defaultProps = {
 
 export class DetailBlock extends Component {
   render() {
-    const { title, text, source } = this.props;
+    const {title, text, source, navigation} = this.props;
 
     return (
-      <TouchableOpacity style= {itemStyle.centerMain}>
+      <TouchableOpacity
+        style={itemStyle.centerMain}
+        onPress={() => {
+          navigation.navigate('newsContent');
+        }}>
         <View style={itemStyle.item}>
-          <Image
-            source={{uri: source}}
-            style={itemStyle.image}
-          />
+          <Image source={{uri: source}} style={itemStyle.image} />
           <Text style={itemStyle.title}>{title}</Text>
           <Text style={itemStyle.text}>{text}</Text>
         </View>
