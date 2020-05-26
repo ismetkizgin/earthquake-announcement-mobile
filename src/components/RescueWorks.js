f30226bd495599979e62a9fc052b68ffcae1e7f5
ismet kizgin <info@ismetkizgin.com>
2020-05-27 00:02:17 +0300

    fix: fix for rescue works page

--- src/components/RescueWorks.js
+++ src/components/RescueWorks.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import {SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
-import {Item} from '.';
-import {commonStyle} from '../util';
+import { SafeAreaView, FlatList, TouchableOpacity, View } from 'react-native';
+import { DetailBlock } from '.';
+import { commonStyle } from '../util';
+import { ScrollView } from 'react-native-gesture-handler';
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'Denizde kurtarma çalışmaları devam etti',
     text:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source: '../assets/ship.jpeg',
+    source: 'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     title: 'Denizde kurtarma çalışmaları devam etti',
     text:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source: '../assets/ship.jpeg',
+    source: 'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Denizde kurtarma çalışmaları devam etti',
     text:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis ',
-    source: '../assets/ship.jpeg',
+    source: 'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
   },
 ];
 
 export const RescueWorks = () => {
   return (
     <SafeAreaView style={commonStyle.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => (
-          <TouchableOpacity>
-            <Item text={item.text} title={item.title} source={item.source} />
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item.id}
-      />
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <DetailBlock text={item.text} title={item.title} source={item.source} />
+          )}
+          keyExtractor={item => item.id}
+        />
     </SafeAreaView>
   );
 };
