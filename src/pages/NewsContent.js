e49fa8b613d8ad090f4fa02862a56a5bed0476cd
alihanakcm <alihanakcm@gmail.com>
2020-05-27 03:04:26 +0300

    fix: news content fixed

--- src/pages/NewsContent.js
+++ src/pages/NewsContent.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import {SafeAreaView, Text, Image, View, StyleSheet} from 'react-native';
-import {commonStyle} from '../util';
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'HABER',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source: '../assets/ship.jpeg',
-  },
-];
+import {Content} from '../components';
 
 export const NewsContent = ({navigation}) => {
   return (
-    <SafeAreaView style={commonStyle.container}>
-      <View>
-        <Image />
-        <Text style={styles.header}>Başlık</Text>
-        <Text style={styles.content}>içerik</Text>
-      </View>
-    </SafeAreaView>
+    <Content
+      title={'Header'}
+      text={'texttet'}
+      source={'https://c1.staticflickr.com/5/4290/34668102354_568ce3fa59_b.jpg'}
+    />
   );
 };
-const styles = StyleSheet.create({
-  header: {
-    fontSize: 20,
-    color: 'white',
-  },
-  content: {
-    fontSize: 15,
-    color: 'white',
-  },
-});
