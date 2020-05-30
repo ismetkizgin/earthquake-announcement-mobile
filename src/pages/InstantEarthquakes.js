85f09a17ccc5731af2b7ae1ac16455b5551ae766
ismet kizgin <info@ismetkizgin.com>
2020-05-31 02:59:40 +0300

    fix: fix for instant earthquakes page

--- src/pages/InstantEarthquakes.js
+++ src/pages/InstantEarthquakes.js
@@ -1,92 +1,58 @@
-import React from 'react';
-import {SafeAreaView, FlatList, Text, View, StyleSheet} from 'react-native';
-import {commonStyle, instantEarthquakes} from '../util';
-const DATA = [
-  {
-    id: '1',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '2',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '3',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '4',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '5',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '6',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-  {
-    id: '7',
-    location: 'Ankara',
-    date: '12.12.2012',
-    time: '15:18:12',
-    depth: '12',
-    size: '10',
-  },
-];
+import React, { useState, useLayoutEffect } from 'react';
+import { SafeAreaView, FlatList, Text, View, StyleSheet } from 'react-native';
+import { commonStyle, instantEarthquakesStyle } from '../util';
+import { InstantEarthquakesList } from '../actions/InstantEarthquakes.actions'
 
 export const InstantEarthquakes = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  fecthData = async () => {
+    setIsLoading(true);
+    InstantEarthquakesList().then(res => {
+      setData(res);
+    }).finally(() => setIsLoading(false));
+  }
+
+  useLayoutEffect(() => {
+    fecthData();
+  }, []);
+
   return (
     <SafeAreaView style={commonStyle.container}>
       <FlatList
-        data={DATA}
-        renderItem={({item}) => (
-          <View style={instantEarthquakes.item}>
-            <View style={instantEarthquakes.location}>
-              <Text style={instantEarthquakes.locationName}>
-                {item.location}
-              </Text>
+        data={data}
+        refreshing={isLoading}
+        onRefresh={fecthData}
+        renderItem={({ item }) => (
+          <View style={instantEarthquakesStyle.item}>
+            <View style={instantEarthquakesStyle.header}>
+              <Text style={instantEarthquakesStyle.cityTitle}>{item.yer}</Text>
             </View>
-            <View style={instantEarthquakes.headers}>
-              <Text style={instantEarthquakes.headersText}>Tarih</Text>
-              <Text style={instantEarthquakes.headersText}>Saat</Text>
-              <Text style={instantEarthquakes.headersText}>Derinlik</Text>
-              <Text style={instantEarthquakes.headersText}>Büyüklük</Text>
+            <View style={instantEarthquakesStyle.timeContent}>
+              <Text style={instantEarthquakesStyle.timeText}>{item.tarih} {item.saat}</Text>
             </View>
-            <View style={instantEarthquakes.data}>
-              <Text>{item.date}</Text>
-              <Text style={instantEarthquakes.time}>{item.time}</Text>
-              <Text style={instantEarthquakes.depth}>{item.depth}</Text>
-              <Text>{item.size}</Text>
+            <View style={instantEarthquakesStyle.content}>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Enlem</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.enlem}</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Boylam</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.boylam}</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Derinlik</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.derinlik} KM</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Büyüklük</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.siddet}</Text>
+              </View>
             </View>
           </View>
         )}
-        keyExtractor={item => item.id}
+        keyExtractor={(item, index) => "key" + index}
       />
     </SafeAreaView>
   );
