25c64d473048b2b54f44f8e1220b6e8440343448
ismet kizgin <info@ismetkizgin.com>
2020-05-31 02:55:56 +0300

    fix: fix for news page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,46 +1,21 @@
-import React from 'react';
-import {SafeAreaView, FlatList} from 'react-native';
-import {DetailBlock} from '../components';
-import {commonStyle} from '../util';
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'HABER',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source: '../assets/ship.jpeg',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'HABER',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source: '../assets/ship.jpeg',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Denizde kurtarma çalışmaları devam etti',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis ',
-    source: '../assets/ship.jpeg',
-  },
-];
+import React, { useState, useEffect } from 'react';
+import { ContentList } from '../components';
+import { NewsList } from '../actions/page.actions'
 
 export const News = ({navigation}) => {
-  return (
-    <SafeAreaView style={commonStyle.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => (
-          <DetailBlock
-            text={item.text}
-            title={item.title}
-            source={item.source}
-            navigation={navigation}
-          />
-        )}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  );
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  fecthData = () => {
+    setIsLoading(true);
+    NewsList().then(res => {
+      setData(res);
+    }).finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
+  return <ContentList navigation={navigation} data={data} refreshing={isLoading} onRefresh={fecthData} />
 };
