ed01dd04cff72767c121f79d8d49f49b51f6646c
ismet kizgin <info@ismetkizgin.com>
2020-05-31 02:57:32 +0300

    fix: fix for rescue works page

--- src/pages/RescueWorks.js
+++ src/pages/RescueWorks.js
@@ -1,49 +1,21 @@
-import React from 'react';
-import {SafeAreaView, FlatList} from 'react-native';
-import {DetailBlock} from '../components';
-import {commonStyle} from '../util';
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Denizde kurtarma çalışmaları devam etti',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source:
-      'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Denizde kurtarma çalışmaları devam etti',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis',
-    source:
-      'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Denizde kurtarma çalışmaları devam etti',
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget augue in velit iaculis fermentum dignissim id est. Maecenas sodales tincidunt est et convallis ',
-    source:
-      'https://cdnuploads.aa.com.tr/uploads/Contents/2019/02/10/thumbs_b_c_7943518a3a2da1f2ddc07ce2633a3672.jpg',
-  },
-];
+import React, { useState, useEffect } from 'react';
+import { ContentList } from '../components';
+import { RescueWorksList } from '../actions/page.actions'
 
-export const RescueWorks = ({navigation}) => {
-  return (
-    <SafeAreaView style={commonStyle.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => (
-          <DetailBlock
-            text={item.text}
-            title={item.title}
-            source={item.source}
-            navigation={navigation}
-          />
-        )}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  );
+export const RescueWorks = ({ navigation }) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  fecthData = () => {
+    setIsLoading(true);
+    RescueWorksList().then(res => {
+      setData(res);
+    }).finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
+  return <ContentList navigation={navigation} data={data} refreshing={isLoading} onRefresh={fecthData} />
 };
