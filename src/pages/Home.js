e32c03a4532de7f2f4918a26448ee02c917e3ab8
ismet kizgin <info@ismetkizgin.com>
2020-06-02 00:54:22 +0300

    fix: fix for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { View, ScrollView, TouchableOpacity, Text, Image, FlatList } from 'react-native';
 import { commonStyle, itemStyle, instantEarthquakesStyle } from '../util'
-import { ContentList } from '../components'
 import { RescueWorksList } from '../actions/page.actions'
+import { InstantEarthquakesPieceList } from '../actions/InstantEarthquakes.actions'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Home = ({ navigation }) => {
-  const [data, setData] = useState(null);
+  const [dataPage, setDataPage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [dataIE, setDataIE] = useState(null);
 
-  fecthData = () => {
+  pageFecthData = () => {
     setIsLoading(true);
     RescueWorksList().then(res => {
-      setData(res);
+      setDataPage(res);
     }).finally(() => setIsLoading(false));
   }
 
+  iEFecthData = async () => {
+    setIsLoading(true);
+    InstantEarthquakesPieceList().then(res => {
+      setDataIE(res);
+    }).finally(() => setIsLoading(false));
+  }
+
+
   useEffect(() => {
-    fecthData();
+    pageFecthData();
+    iEFecthData();
   }, []);
 
   return (
-    <ScrollView style={commonStyle.container}>
+    <SafeAreaView style={commonStyle.container}>
       <FlatList
-        data={data}
-        onRefresh={fecthData}
+        data={dataPage}
+        onRefresh={pageFecthData}
         refreshing={isLoading}
         horizontal={true}
         renderItem={({ item }) => (
@@ -33,19 +44,51 @@ export const Home = ({ navigation }) => {
               navigation.navigate('Detail', item);
             }}>
             <View style={[itemStyle.item, { alignItems: "center" }]}>
-              <Image source={{ uri: item.PagePicture }} style={itemStyle.image} />
+              <Image source={{ uri: item.PagePicture }} style={[itemStyle.image, itemStyle.homeImage]} />
               <Text style={itemStyle.title}>{item.PageTitle}</Text>
             </View>
           </TouchableOpacity>
         )}
         keyExtractor={(item) => item.PageID.toString()}
       />
-      <View style={instantEarthquakesStyle.container}>
-        <Text>asdasd</Text>
-        <Text>asdasd</Text>
-        <Text>asdasd</Text>
-      </View>
-    </ScrollView>
+
+      <FlatList
+        data={dataIE}
+        refreshing={isLoading}
+        onRefresh={iEFecthData}
+        horizontal={true}
+        renderItem={({ item }) => (
+          <View style={instantEarthquakesStyle.item}>
+            <View style={instantEarthquakesStyle.header}>
+              <Text style={instantEarthquakesStyle.cityTitle}>{item.yer}</Text>
+            </View>
+            <View style={instantEarthquakesStyle.timeContent}>
+              <Text style={instantEarthquakesStyle.timeText}>{item.tarih} {item.saat}</Text>
+            </View>
+            <View style={instantEarthquakesStyle.content}>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Enlem</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.enlem}</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Boylam</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.boylam}</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Derinlik</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.derinlik} KM</Text>
+              </View>
+              <View>
+                <Text style={instantEarthquakesStyle.contentTitle}>Büyüklük</Text>
+                <Text style={instantEarthquakesStyle.contentText}>{item.siddet}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item, index) => "key" + index}
+      />
+
+    </SafeAreaView>
   );
 };
 
