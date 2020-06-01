6649891f5ae999654c454340e41b7d3dbbe74fc4
ismet kizgin <info@ismetkizgin.com>
2020-06-01 21:57:02 +0300

    fix: fix for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,51 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, TouchableOpacity, Text, Image, FlatList } from 'react-native';
+import { commonStyle, itemStyle, instantEarthquakesStyle } from '../util'
+import { ContentList } from '../components'
+import { RescueWorksList } from '../actions/page.actions'
 
-export const Home = ({navigation}) => {
+export const Home = ({ navigation }) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  fecthData = () => {
+    setIsLoading(true);
+    RescueWorksList().then(res => {
+      setData(res);
+    }).finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView style={commonStyle.container}>
+      <FlatList
+        data={data}
+        onRefresh={fecthData}
+        refreshing={isLoading}
+        horizontal={true}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={itemStyle.centerMain}
+            onPress={() => {
+              navigation.navigate('Detail', item);
+            }}>
+            <View style={[itemStyle.item, { alignItems: "center" }]}>
+              <Image source={{ uri: item.PagePicture }} style={itemStyle.image} />
+              <Text style={itemStyle.title}>{item.PageTitle}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.PageID.toString()}
+      />
+      <View style={instantEarthquakesStyle.container}>
+        <Text>asdasd</Text>
+        <Text>asdasd</Text>
+        <Text>asdasd</Text>
+      </View>
+    </ScrollView>
   );
 };
+
